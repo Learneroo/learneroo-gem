ab59329c759ba0e176b2de617f3860fc36a3325e
Ariel Krakowski <arikrak@gmail.com>
2015-07-17 13:55:57 -0400

    Report results to external site

--- lib/learneroo_gem.rb
+++ lib/learneroo_gem.rb
@@ -3,6 +3,8 @@ require "minitest"
 
 module Minitest
 	class ProgressReporter < Reporter
+    @@tests = 0
+
 		def record result # :nodoc:
 			if options[:verbose]
 				io.print "%s#%s = %.2f s = " % [result.class, result.name, result.time]
@@ -11,9 +13,31 @@ module Minitest
 			end
 			if result.result_code == '.'
 				puts '✔'
-			else
+        @@tests += 1
+      else
 				puts result.result_code
-			end
-		end
+      end
+      report_results(result)
+    end
+
+    # report results if wrong or last one.
+    def report_results(result)
+      api_token = ENV["API_TOKEN"]
+      #test_number = result.name[5..6]
+      if api_token && (result.result_code != '.' || @@tests == 42) #adjust
+        puts "Reporting results"
+        test_url = ENV["API_URL"]
+
+        require "uri"
+        require "net/http"
+
+        params = {'test_number'=> @@tests,
+                  'api_token' => api_token
+                }
+        x = Net::HTTP.post_form(URI.parse(test_url), params)
+        puts x.body
+      end
+    end
+
 	end
 end
--- lib/learneroo_gem/version.rb
+++ lib/learneroo_gem/version.rb
@@ -1,3 +1,3 @@
 module LearnerooGem
-  VERSION = "0.0.1"
+  VERSION = "0.0.2.beta"
 end
